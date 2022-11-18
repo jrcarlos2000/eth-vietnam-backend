f128a4f41a8c49d1313755d4616d9d67d7744562
CarlosR <carlos.ramos@grupoimportia.com>
2022-11-18 22:05:49 +0700

    feat : added setup for db

--- server.ts
+++ server.ts
@@ -2,34 +2,61 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import morganBody from "morgan-body";
 import bodyParser from "body-parser";
+import {MongoClient} from  "mongodb";
 const cors = require("cors");
 
+dotenv.config();
 const corsOptions = {
   origin: "http://localhost:3000",// TODO : Add custom domain
   optionsSuccessStatus: 200,
 };
 
-const winston = require("winston");
-const consoleTransport = new winston.transports.Console();
-dotenv.config();
-
-// const myWinstonOptions = {
-//   transports: [consoleTransport],
-// };
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 9000;
+
 // parse JSON and others
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 
-
-
 // log all requests and responses
 morganBody(app, { logAllReqHeader: true, maxBodyLength: 5000 });
 
+//connect to db 
 
+let cachedClient = null;
+let cachedDb:any = null;
+
+const connectToDb = async () => {
+
+  if(cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(process.env.DATABASE_URL!,{})
+
+  const db = client.db("facets");
+  cachedDb = db;
+  cachedClient = client;
+
+  return db
+
+}
+
+app.post("/add-facet", async (req : Request, res : Response) => {
+  const {name} = req.body;
+  const db = await connectToDb();
+  const facet = await db.collection("facets").insertOne({name});
+  res.status(200).end();
+})
+
+app.get("/facets", async (req : Request, res: Response) => {
+  const db = await connectToDb();
+  const facets = await db.collection("facets").find({}).toArray(); 
+  res.status(200).send({
+    facets
+  })
+})
 
 app.post("/test", async (req: Request, res: Response) => {
   // const { param1 , param2} = req.body;

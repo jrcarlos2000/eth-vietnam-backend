df1b3f405d429d6a35a3d7f6dfe141494e4541ef
CarlosR <carlos.ramos@grupoimportia.com>
2022-12-08 17:49:52 +0800

    dorachacks

--- server.ts
+++ server.ts
@@ -52,7 +52,7 @@ const connectToDb = async () => {
 
 let busy = false;
 
-cron.schedule("*/4 * * * *", async () => {
+cron.schedule("*/9 * * * *", async () => {
   if (!busy) {
     console.log("running a task every 10 minutes");
     const promises = [];
